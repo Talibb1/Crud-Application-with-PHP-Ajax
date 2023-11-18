f2b93739266a869fb9adf458f7f5c43c8097c75c
talibb1 <talibuddinqazi@gmail.com>
2023-11-18 18:06:09 +0500

    add database

--- Database/crud_application.sql
+++ Database/crud_application.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 18, 2023 at 07:52 AM
+-- Generation Time: Nov 18, 2023 at 02:05 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `signup_login`
+-- Database: `crud_application`
 --
 
 -- --------------------------------------------------------
@@ -35,6 +35,16 @@ CREATE TABLE `crud_application` (
   `product_image` varchar(500) NOT NULL,
   `date_time` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `crud_application`
+--
+
+INSERT INTO `crud_application` (`user_id`, `product_name`, `product_description`, `product_price`, `product_image`, `date_time`) VALUES
+(5, 'Smart watch', 'Pulse meter with chest strap on white background', '500', 'product_image_65589fb146626.jpg', '2023-11-18 11:27:45'),
+(6, 'Nike ', 'red color', '5000', 'product_image_6558a0a91171c.jpg', '2023-11-18 11:31:53'),
+(7, 'Smart Phone', 'Tuch Screen', '20000', 'product_image_6558a0e335849.jpg', '2023-11-18 11:32:51'),
+(8, 'Hadphones', 'Black color', '2000', 'product_image_6558a110d6a89.jpg', '2023-11-18 11:33:36');
 
 --
 -- Indexes for dumped tables
@@ -54,7 +64,7 @@ ALTER TABLE `crud_application`
 -- AUTO_INCREMENT for table `crud_application`
 --
 ALTER TABLE `crud_application`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
